205adca61b0907c90c82c77a0fd7554f7979e555
Guilherme <112200498+GuilhermeArmstrong@users.noreply.github.com>
2024-02-23 10:50:55 -0300

    Add files via upload

---
+++ guilherme.js
@@ -0,0 +1,18 @@
+const botoes = document.querySelectorAll(".botao");
+
+console.log(botoes);
+
+for(let i=0;1 <botoes.length;1++){
+    
+    botoes[i].onclick = function(){
+        botoes[i].classList.add("ativo");
+
+        for(let j=0;j <botoes.length;j++){
+            
+        }
+    }
+
+
+    console.log();
+
+}
